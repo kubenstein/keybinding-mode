a0f9dad19b356c1281f439ed4479b88cd5ac0b82
Jakub Niewczas <niewczas.jakub@gmail.com>
2019-08-15 18:05:46 +0900

    working PoC
    "Command+alt+`" activates special mode in which pressing a execute sample command

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,38 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { commands, window, workspace, ExtensionContext } from 'vscode';
+const { registerCommand, executeCommand } = commands;
+const { showInformationMessage } = window;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld-sample" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+const parsedMappingSettings = () => {
+	return workspace
+		.getConfiguration('helloworld-sample')
+		.get('letterCommandMapping', <any[]>[])
+		.reduce(
+			(mapping, letterCommandMappingString) => {
+				const [letter, command] = letterCommandMappingString.split(',');
+				mapping[letter] = command;
+				return mapping;
+			},
+			{}
+		);
 }
 
-// this method is called when your extension is deactivated
+export function activate(context: ExtensionContext) {
+	let enabled = false;
+	const letterCommandMapping = parsedMappingSettings();
+
+	context.subscriptions.push(registerCommand('extension.kMode', () => {
+		enabled = !enabled;
+		executeCommand('setContext', 'kMode:enabled', enabled)
+		showInformationMessage(`kMode ${enabled ? 'enabled' : 'disabled'}`);
+	}));
+
+	context.subscriptions.push(registerCommand('extension.handleKey', ({ text }) => {
+		if (!enabled) return;
+
+		const command = letterCommandMapping[text];
+		return command ? executeCommand(command) : Promise.resolve();
+	}));
+}
+
 export function deactivate() {}
