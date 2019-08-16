80210dabcedb096a9188379fb3e1f7e2d8269bce
Jakub Niewczas <niewczas.jakub@gmail.com>
2019-08-16 14:47:49 +0900

    convert tabs -> spaces

--- src/extension.ts
+++ src/extension.ts
@@ -4,31 +4,31 @@ const { showInformationMessage } = window;
 
 
 const commandForLetter = (letter: string) => (
-	(workspace
-		.getConfiguration('keybindingMode')
-		.get('letterCommandMapping', <string[]>[])
-		.find(letterCommandMappingString => letterCommandMappingString[0] === letter) || ''
-	)
-	.split(',')[1]
+  (workspace
+    .getConfiguration('keybindingMode')
+    .get('letterCommandMapping', <string[]>[])
+    .find(letterCommandMappingString => letterCommandMappingString[0] === letter) || ''
+  )
+  .split(',')[1]
 );
 
 export function activate(context: ExtensionContext) {
-	let enabled = false;
+  let enabled = false;
 
-	context.subscriptions.push(registerCommand('keybindingMode.toggle', () => {
-		enabled = !enabled;
-		executeCommand('setContext', 'keybindingMode:enabled', enabled);
-		showInformationMessage(`keybindingMode ${enabled ? 'enabled' : 'disabled'}`);
-	}));
+  context.subscriptions.push(registerCommand('keybindingMode.toggle', () => {
+    enabled = !enabled;
+    executeCommand('setContext', 'keybindingMode:enabled', enabled);
+    showInformationMessage(`keybindingMode ${enabled ? 'enabled' : 'disabled'}`);
+  }));
 
-	context.subscriptions.push(registerCommand('keybindingMode.handleKey', ({ text: letter }) => {
-		if (!enabled) return;
+  context.subscriptions.push(registerCommand('keybindingMode.handleKey', ({ text: letter }) => {
+    if (!enabled) return;
 
-		const command = commandForLetter(letter);
-		if (command) {
-			executeCommand(command);
-		}
-	}));
+    const command = commandForLetter(letter);
+    if (command) {
+      executeCommand(command);
+    }
+  }));
 }
 
 export function deactivate() {}
